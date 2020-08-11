33a4dfbb2032c767ce788128536ed5864f0ac6e3
jbh <jang2162_@naver.com>
2020-08-12 01:42:05 +0900

    인증 쿠키 버그 개선

--- src/app/common/auth/auth.provider.ts
+++ src/app/common/auth/auth.provider.ts
@@ -1,5 +1,4 @@
 import {DatabaseTransactionProvider} from '@/app/common/database/database.transaction.provider';
-import {AccessToken} from '@/generated-models';
 import {Injectable} from '@graphql-modules/di';
 import {ApolloError} from 'apollo-server-errors';
 import {Response} from 'express';
@@ -32,28 +31,34 @@ export class AuthProvider {
 
     async authentication(response: Response, uid: number, salt: string) {
         const {trx, release} = await this.dbTran.getTransaction();
-        const refreshKey = uuid4().replace(/-/g, '');
-        const accessKey = uuid4().replace(/-/g, '');
-        const roles = (await trx('user_role').where('user_id', uid)).map((item: any) => item.role_id);
-        await trx('auth_token').insert({
-            user_id: uid,
-            refresh_key: refreshKey,
-            access_key: accessKey,
-            salt,
-        });
-        const accessToken = sign({
-            uid,
-            rol: roles,
-            rfk: refreshKey
-        }, this.secret, {
-            issuer: this.issuer,
-            subject: 'ACCESS_TOKEN',
-            expiresIn: this.expiredIn,
-            jwtid: accessKey,
-            noTimestamp: true
-        });
+        let accessToken;
+        try {
+            const refreshKey = uuid4().replace(/-/g, '');
+            const accessKey = uuid4().replace(/-/g, '');
+            const roles = (await trx('user_role').where('user_id', uid)).map((item: any) => item.role_id);
+            await trx('auth_token').insert({
+                user_id: uid,
+                refresh_key: refreshKey,
+                access_key: accessKey,
+                salt,
+            });
+            accessToken = sign({
+                uid,
+                rol: roles,
+                rfk: refreshKey
+            }, this.secret, {
+                issuer: this.issuer,
+                subject: 'ACCESS_TOKEN',
+                expiresIn: this.expiredIn,
+                jwtid: accessKey,
+                noTimestamp: true
+            });
+            await release();
+        } catch (e) {
+            await release();
+            throw e;
+        }
 
-        await release();
         response.cookie('token', accessToken, {
             secure: this.isCookieSecure,
             domain: this.cookieDomain,
@@ -61,57 +66,64 @@ export class AuthProvider {
             httpOnly: true,
             sameSite: 'Lax'
         });
+
     }
 
     async refresh(response: Response, accessTokenPayload: IAccessToken) {
         const {trx, release} = await this.dbTran.getTransaction();
+        let token;
+        try {
+            const authData = await trx('auth_token').first([
+                'user_id',
+                this.dbTran.knex.raw('EXTRACT(epoch FROM (NOW() - last_date))::int refresh_interval'),
+                'access_key',
+                'salt'
+            ]).where('refresh_key', accessTokenPayload.rfk)
+                .where('disabled', 0);
+            const saltRow = await trx('user').first(this.dbTran.knex.raw('SUBSTR(password, 0, 30) salt'))
+                .where('id', accessTokenPayload.uid);
 
-        const authData = await trx('auth_token').first([
-            'user_id',
-            this.dbTran.knex.raw('EXTRACT(epoch FROM (NOW() - last_date))::int refresh_interval'),
-            'access_key',
-            'salt'
-        ]).where('refresh_key', accessTokenPayload.rfk)
-            .where('disabled', 0);
-        const saltRow = await trx('user').first(this.dbTran.knex.raw('SUBSTR(password, 0, 30) salt'))
-            .where('id', accessTokenPayload.uid);
+            if (!(saltRow?.salt) || !authData || authData.user_id !== accessTokenPayload.uid) {
+                throw new ApolloError('', 'UNKNOWN_ERROR');
+            }
 
-        if (!(saltRow?.salt) || !authData || authData.user_id !== accessTokenPayload.uid) {
-            throw new ApolloError('', 'UNKNOWN_ERROR');
-        }
+            if (authData.salt !== saltRow.salt) {
+                throw new ApolloError('', 'SALT_VALUE_CHANGED');
+            }
 
-        if (authData.salt !== saltRow.salt) {
-            throw new ApolloError('', 'SALT_VALUE_CHANGED');
-        }
+            if (authData.refresh_interval > this.refreshExpiredIn) {
+                throw new ApolloError('', 'REFRESH_TOO_LATE');
+            }
 
-        if (authData.refresh_interval > this.refreshExpiredIn) {
-            throw new ApolloError('', 'REFRESH_TOO_LATE');
-        }
+            if (authData.access_key !== accessTokenPayload.jti) {
+                await trx('auth_token').update({
+                    disabled: 1
+                }).where('refresh_key', accessTokenPayload.rfk);
+                throw new ApolloError('', 'ACCESS_KEY_IS_OLD');
+            }
 
-        if (authData.access_key !== accessTokenPayload.jti) {
+            const accessKey = uuid4().replace(/-/g, '');
+            const roles = (await trx('user_role').where('user_id', accessTokenPayload.uid)).map((item: any) => item.role_id);
             await trx('auth_token').update({
-                disabled: 1
+                access_key: accessKey,
+                last_date: this.dbTran.knex.fn.now()
             }).where('refresh_key', accessTokenPayload.rfk);
-            throw new ApolloError('', 'ACCESS_KEY_IS_OLD');
+            token = sign({
+                uid: accessTokenPayload.uid,
+                rol: roles,
+                rfk: accessTokenPayload.rfk,
+            }, this.secret, {
+                issuer: this.issuer,
+                subject: 'ACCESS_TOKEN',
+                expiresIn: this.expiredIn,
+                jwtid: accessKey,
+                noTimestamp: true
+            });
+        } catch (e) {
+            await release();
+            throw e;
         }
 
-        const accessKey = uuid4().replace(/-/g, '');
-        const roles = (await trx('user_role').where('user_id', accessTokenPayload.uid)).map((item: any) => item.role_id);
-        await trx('auth_token').update({
-            access_key: accessKey,
-            last_date: this.dbTran.knex.fn.now()
-        }).where('refresh_key', accessTokenPayload.rfk);
-        const token = sign({
-            uid: accessTokenPayload.uid,
-            rol: roles,
-            rfk: accessTokenPayload.rfk,
-        }, this.secret, {
-            issuer: this.issuer,
-            subject: 'ACCESS_TOKEN',
-            expiresIn: this.expiredIn,
-            jwtid: accessKey,
-            noTimestamp: true
-        });
         await release();
         response.cookie('token', token, {
             secure: this.isCookieSecure,
